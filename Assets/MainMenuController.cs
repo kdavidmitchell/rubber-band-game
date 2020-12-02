2d4e38aaa859ce2d1ed558e768c4bfa249a54793
Kyle Mitchell <kdavidmitchell@gmail.com>
2020-12-02 14:06:45 -0800

    adding assets and impl for main menu
    adding assets and impl for main menu

---
+++ Assets/MainMenuController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuController : MonoBehaviour
+{
+    public void playGame() {
+        SceneManager.LoadScene("Demo");
+    }
+ 
+    public void exitGame() {
+        Application.Quit();
+    }
+}
