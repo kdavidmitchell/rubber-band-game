3b4870c8ef78e1ab75983debecbbab0ff7324e8f
Kyle Mitchell <kdavidmitchell@gmail.com>
2020-12-03 16:37:49 -0800

    adding timed spawner to demo scene
    adding timed spawner to demo scene

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -4,15 +4,30 @@ using UnityEngine;
 
 public class EnemySpawner : MonoBehaviour
 {
+    public GameObject enemy;
+    private float timer = 5f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        // Spawn first enemy before first frame update.
+        SpawnEnemy();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // Timer that determines spawn rate.
+        timer -= Time.deltaTime;
+        if (timer <= 0) {
+            timer = 5f;
+            SpawnEnemy();
+        }
+    }
+
+    // Spawns enemy prefab in current position with no rotation.
+    void SpawnEnemy() {
+        Vector3 currentPos = new Vector3(transform.position.x, transform.position.y, 0);
+        Instantiate(enemy, currentPos, Quaternion.identity);
     }
 }
